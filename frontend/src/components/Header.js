384d678cfba5e4a3d389bee73443c892deb8fe2b
cutratefuture <christian@cutratefuture.com>
2020-12-01 17:02:46 -0800

    2nd half of vid

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,15 +4,17 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='/cart'>Cart </Nav.Link>
-            <Nav.Link href='/login'>Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>PROSHOP</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>Cart </Nav.Link>
+              <Nav.Link href='/login'>Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   )
