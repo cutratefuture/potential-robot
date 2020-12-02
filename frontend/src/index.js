1e479dfb6d5f5efe4baef9b995181b1820302202
cutratefuture <christian@cutratefuture.com>
2020-12-02 15:11:56 -0800

    vid6 homescreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './bootstrap.minhttps://github.com/cutratefuture/potential-robot.git.css'
+import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
