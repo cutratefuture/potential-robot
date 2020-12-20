372fdcf4f948a34d92dd6f8c4d68cf84de46457a
cutratefuture <christian@cutratefuture.com>
2020-12-20 11:11:41 -0800

    import from state

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,7 +2,8 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
-} from '../constants/productConstants'
+} from '../constants/productConstants.js'
+
 export const productListReducer = (state = {products: []}, action) => {
     switch(action.type) {
         case PRODUCT_LIST_REQUEST:
